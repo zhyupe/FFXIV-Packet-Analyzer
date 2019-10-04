18ccf5345dac0a881ac5d4b387e2784352a2586b
Innpaul Zhang <zyp108421@gmail.com>
2019-10-04 22:48:56 +0800

    fix(ui): Fix high-dpi supporting fix(db): Fix DB in ACT

---
+++ ControlExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Windows.Forms;
+
+namespace PacketAnalyzer
+{
+    public static class ControlExtensions
+    {
+        public static void DoubleBuffering(this Control control, bool enable)
+        {
+            var method = typeof(Control).GetMethod("SetStyle", BindingFlags.Instance | BindingFlags.NonPublic);
+            method.Invoke(control, new object[] { ControlStyles.OptimizedDoubleBuffer, enable });
+        }
+    }
+}
